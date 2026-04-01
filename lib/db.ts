0cf0944a697edab836a9ff9c13eac5dd88542e66
Mrunali Bind <mrunalibind123@gmail.com>
2026-04-01 08:50:41 +0530

    Mongo error fixed

--- lib/db.ts
+++ lib/db.ts
@@ -6,26 +6,31 @@ if (!MONGO_URI) {
   throw new Error("Please define MONGO_URI");
 }
 
-declare global {
-  var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+const globalWithMongoose = globalThis as typeof globalThis & {
+  mongoose?: MongooseCache;
+};
+
+let cached = globalWithMongoose.mongoose;
+
+if (!cached) {
+  cached = globalWithMongoose.mongoose = {
+    conn: null,
+    promise: null,
   };
 }
 
-let cached: any = global.mongoose;
+export const connectDB = async () => {
+  if (cached!.conn) return cached!.conn;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGO_URI);
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
+};
